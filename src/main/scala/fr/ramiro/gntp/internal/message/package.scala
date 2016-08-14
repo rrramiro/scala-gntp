66ec8f2c66c08183083dc861dd79b56b7ddb4653
rrramiro <ramiro.sarmiento@gmail.com>
2016-08-14 22:44:11 +0100

    Remove case class BinarySection

--- src/main/scala/fr/ramiro/gntp/internal/message/package.scala
+++ src/main/scala/fr/ramiro/gntp/internal/message/package.scala
@@ -3,9 +3,13 @@ package fr.ramiro.gntp.internal
 import java.awt.image.RenderedImage
 import java.io.{ ByteArrayOutputStream, InputStream }
 import java.net.URI
+import java.security.MessageDigest
 import java.text.{ DateFormat, SimpleDateFormat }
 import java.util.Date
 import javax.imageio.ImageIO
+
+import fr.ramiro.gntp.GntpPassword
+import fr.ramiro.gntp.util.Hex
 
 import scala.language.implicitConversions
 
@@ -17,61 +21,58 @@ package object message {
     def toHeader: String
   }
 
-  sealed trait HeaderValue extends HeaderObject
-
   trait BinaryHeaderValue extends HeaderObject {
-    def binarySection: BinarySection
+    val byteArray: Array[Byte]
+    lazy val uniqueId: String = {
+      val digest = MessageDigest.getInstance(GntpPassword.BINARY_HASH_FUNCTION)
+      digest.update(byteArray)
+      Hex.toHexadecimal(digest.digest)
+    }
+    def toHeader: String = GntpMessage.BINARY_SECTION_PREFIX + uniqueId
   }
 
   object HeaderSpacer extends HeaderObject {
     def toHeader: String = GntpMessage.SEPARATOR
   }
 
-  case class HeaderString(value: String) extends HeaderValue {
-    override def toHeader: String = value.replaceAll("\r\n", "\n")
+  implicit def toHeaderString(field: String): HeaderObject = new HeaderObject {
+    override def toHeader: String = field.replaceAll("\r\n", "\n")
   }
-  case class HeaderNumber(value: Number) extends HeaderValue {
-    override def toHeader: String = value.toString
+  implicit def toHeaderNumber(field: Number): HeaderObject = new HeaderObject {
+    override def toHeader: String = field.toString
   }
-  case class HeaderBoolean(value: Boolean) extends HeaderValue {
-    override def toHeader: String = value.toString.toLowerCase.capitalize
+  implicit def toHeaderLong(field: Long): HeaderObject = toHeaderNumber(field)
+  implicit def toHeaderInt(field: Int): HeaderObject = toHeaderNumber(field)
+  implicit def toHeaderBoolean(field: Boolean): HeaderObject = new HeaderObject {
+    override def toHeader: String = field.toString.toLowerCase.capitalize
   }
-  case class HeaderDate(value: Date) extends HeaderValue {
-    override def toHeader: String = dateFormat.format(value)
+  implicit def toHeaderDate(field: Date): HeaderObject = new HeaderObject {
+    override def toHeader: String = dateFormat.format(field)
   }
-  case class HeaderUri(value: URI) extends HeaderValue {
-    override def toHeader: String = value.toString
+  implicit def toHeaderUri(field: URI): HeaderObject = new HeaderObject {
+    override def toHeader: String = field.toString
   }
-  case class HeaderInputStream(value: InputStream) extends BinaryHeaderValue {
-    val binarySection = new BinarySection(Stream.continually(value.read)
-      .takeWhile(_ != -1)
-      .map(_.toByte).toArray)
-    value.close() //TODO check
-
-    override def toHeader: String = GntpMessage.BINARY_SECTION_PREFIX + binarySection.id
+  implicit def toHeaderArrayBytes(field: Array[Byte]): BinaryHeaderValue = new BinaryHeaderValue {
+    override val byteArray: Array[Byte] = field
   }
-  case class HeaderArrayBytes(value: Array[Byte]) extends BinaryHeaderValue {
-    val binarySection = new BinarySection(value)
-    override def toHeader: String = GntpMessage.BINARY_SECTION_PREFIX + binarySection.id
-  }
-  case class HeaderRenderedImage(value: RenderedImage) extends BinaryHeaderValue {
-    private val output: ByteArrayOutputStream = new ByteArrayOutputStream
-    if (!ImageIO.write(value, GntpMessage.IMAGE_FORMAT, output)) {
-      throw new IllegalStateException("Could not read icon data")
+  implicit def toHeaderInputStream(field: InputStream): BinaryHeaderValue = new BinaryHeaderValue {
+    val byteArray: Array[Byte] = try {
+      Stream.continually(field.read)
+        .takeWhile(_ != -1)
+        .map(_.toByte).toArray
+    } finally {
+      field.close()
     }
-    val binarySection = new BinarySection(output.toByteArray)
-    override def toHeader: String = GntpMessage.BINARY_SECTION_PREFIX + binarySection.id
   }
 
-  implicit def toHeaderString(field: String): HeaderValue = HeaderString(field)
-  implicit def toHeaderNumber(field: Number): HeaderValue = HeaderNumber(field)
-  implicit def toHeaderLong(field: Long): HeaderValue = HeaderNumber(field)
-  implicit def toHeaderInt(field: Int): HeaderValue = HeaderNumber(field)
-  implicit def toHeaderBoolean(field: Boolean): HeaderValue = HeaderBoolean(field)
-  implicit def toHeaderDate(field: Date): HeaderValue = HeaderDate(field)
-  implicit def toHeaderUri(field: URI): HeaderValue = HeaderUri(field)
-  implicit def toHeaderInputStream(field: InputStream): BinaryHeaderValue = HeaderInputStream(field)
-  implicit def toHeaderArrayBytes(field: Array[Byte]): BinaryHeaderValue = HeaderArrayBytes(field)
-  implicit def toHeaderRenderedImage(field: RenderedImage): BinaryHeaderValue = HeaderRenderedImage(field)
+  implicit def toHeaderRenderedImage(field: RenderedImage): BinaryHeaderValue = new BinaryHeaderValue {
+    val byteArray = {
+      val output: ByteArrayOutputStream = new ByteArrayOutputStream
+      if (!ImageIO.write(field, GntpMessage.IMAGE_FORMAT, output)) {
+        throw new IllegalStateException("Could not read icon data")
+      }
+      output.toByteArray
+    }
+  }
 
 }
